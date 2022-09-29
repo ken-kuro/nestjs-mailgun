4ad2372e405e4091ae4ccf31ceec26dfe3be6402
ken-kuro <hieuhm261@gmail.com>
2022-09-29 19:37:45 +0700

    fix: add missing import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-export { MailgunModule } from './mailgun.module';
-export { OptionsAsync } from './interfaces';
-export { MailgunService, MailgunError } from './mailgun.service';
+export {MailgunModule} from './mailgun.module';
+export {OptionsAsync} from './interfaces';
+export {MailgunService, MailgunError} from './mailgun.service';
 export {
-  MailgunMessageData,
-  MessagesSendResult,
+    MailgunMessageData,
+    MessagesSendResult,
 } from 'mailgun.js/interfaces/Messages';
-export { default as APIResponse } from 'mailgun.js/interfaces/ApiResponse';
-export { default as APIErrorOptions } from 'mailgun.js/interfaces/APIErrorOptions';
+export {
+    DomainTemplateData, DomainTemplatesQuery, ListDomainTemplatesResult,
+    TemplateQuery,
+    UpdateOrDeleteDomainTemplateResult
+} from 'mailgun.js/interfaces/DomainTemplates';
+export {DomainTemplateItem} from "mailgun.js/domainsTemplates";
+export {default as APIResponse} from 'mailgun.js/interfaces/ApiResponse';
+export {default as APIErrorOptions} from 'mailgun.js/interfaces/APIErrorOptions';
